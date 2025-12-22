c5a9303c608c934d5ab563997c985295430a10e2
Ivan Nikcevic <nikcevici13@gmail.com>
2025-12-22 23:59:19 +0100

    reading file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+#include <fstream>
 
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int main(int argc, char* argv[]) {
+
+  if(argc!=2) {
+    std::cerr << "Incorrect usage. Correct usage is..."<< std::endl;
+    std::cerr << "hydro <input.hy>" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::fstream input(argv[1], std::ios::in);
+
+  std::cout << argv[1] << std::endl;
+  return EXIT_SUCCESS;
 }
