7d556225af7c6e56b64c8d69ce43ac463af600c6
Ivan Nikcevic <nikcevici13@gmail.com>
2025-12-23 11:05:11 +0100

    tokens to asm

--- src/main.cpp
+++ src/main.cpp
@@ -65,6 +65,27 @@ std::vector<Token> tokenize(const std::string& str) {
   return tokens;
 };
 
+std::string tokens_to_asm(const std::vector<Token>& tokens) {
+  std::stringstream output;
+  output << "global _start\n_start:\n";
+
+  for(int i = 0; i < tokens.size(); i++) {
+    const Token& token = tokens.at(i);
+    if(token.type == TokenType::_return) {
+      if(i+1 < tokens.size() && tokens.at(i+1).type == TokenType::int_lit) {
+        if(i+2 < tokens.size() && tokens.at(i+2).type == TokenType::semi) {
+          output << " mov rax, 60\n";
+          output << " mov rdi, " << tokens.at(i+1).value.value() << "\n";
+          output << " syscall\n";
+        } 
+
+      } 
+    }
+  }
+
+  return output.str();
+}
+
 
 
 int main(int argc, char* argv[]) {
@@ -83,7 +104,8 @@ int main(int argc, char* argv[]) {
     contents = contents_stream.str();
   }
 
-  std::vector<Token> thing = tokenize(contents);
+  std::vector<Token> tokens = tokenize(contents);
+  std::cout << tokens_to_asm(tokens); << str::endl;
 
 
   return EXIT_SUCCESS;
